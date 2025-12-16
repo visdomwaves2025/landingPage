44fa7223bfebe667e08048e7d4dc331c6a8855da
krant <kranthichowdary2435@gmail.com>
2025-12-16 21:54:14 +0530

    feat: Implement initial application structure, education-focused pages, authentication, payment flows, and update project configurations.

--- visdom-waves/visdomwaves-website/vite.config.js
+++ visdom-waves/visdomwaves-website/vite.config.js
@@ -3,7 +3,6 @@ const react = require('@vitejs/plugin-react');
 const path = require('path');
 
 module.exports = defineConfig({
-    base: '/test/',
     plugins: [react()],
     resolve: {
         alias: {
